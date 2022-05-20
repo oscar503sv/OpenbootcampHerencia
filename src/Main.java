469a209852a92f44b24b73b08c4d974ae86b001c
Oscar Hernandez <ozkar@outlook.com>
2022-05-20 15:00:14 -0600

    clase trabajador agregada

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,23 @@ public class Main {
         cliente.setTelefono("2264-8975");
         cliente.setCredito(12000);
 
-        System.out.println("Nombre: "+cliente.getNombre());
+        System.out.println("****CLASE CLIENTE****");
+        System.out.println("Nombre de cliente: "+cliente.getNombre());
         System.out.println("Edad: "+cliente.getEdad());
         System.out.println("Telefono: "+cliente.getTelefono());
-        System.out.println("Credito otorgado: "+cliente.getCredito());
+        System.out.println("Credito otorgado: "+cliente.getCredito()+"\n");
 
+        Trabajador trabajador = new Trabajador();
 
+        trabajador.setNombre("Henry");
+        trabajador.setEdad(36);
+        trabajador.setTelefono("6458-7894");
+        trabajador.setSalario(1675.10);
+
+        System.out.println("****CLASE TRABAJADOR****");
+        System.out.println("Nombre de trabajador: "+trabajador.getNombre());
+        System.out.println("Edad: "+trabajador.getEdad());
+        System.out.println("Telefono: "+trabajador.getTelefono());
+        System.out.println("Salario: "+trabajador.getSalario());
     }
 }
---
+++ src/Trabajador.java
@@ -0,0 +1,13 @@
+public class Trabajador extends Persona {
+
+    private double salario;
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
